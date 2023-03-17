06d6b1a94e1c1c31a180f390182d732251d1afa2
Ruhul Muazzam Rafat <rmrafat420@gmail.com>
2023-03-17 11:28:17 +0600

    feature section added

---
+++ src/Components/Features/Card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Card = ({title,des}) => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Card;
---
+++ src/Components/Features/Features.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import Title from '../Layout/Title';
+import Card from './Card';
+import {AiFillAppstore,SiProgress,FaMobile,SiAntdesign,FaGlobe} from 'react-icons/fa'
+
+const Features = () => {
+    return (
+        <section id='feature' className='w-full py-20 border-b-[1px] border-b-[black]'>
+            <Title title={"feature"} des={"What I Do"} />
+
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 xl:gap-20'>
+            <Card
+          title="Business Stratagy"
+          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque soluta
+          hic consequuntur eum repellendus ad."
+         
+        />
+        <Card
+          title="App Development"
+          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque soluta
+          hic consequuntur eum repellendus ad."
+          icon={<AiFillAppstore />}
+        />
+        <Card
+          title="SEO Optimisation"
+          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque soluta
+          hic consequuntur eum repellendus ad."
+          icon={<SiProgress />}
+        />
+        <Card
+          title="Mobile Development"
+          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque soluta
+          hic consequuntur eum repellendus ad."
+          icon={<FaMobile />}
+        />
+        <Card
+          title="UX Design"
+          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque soluta
+          hic consequuntur eum repellendus ad."
+          icon={<SiAntdesign />}
+        />
+        <Card
+          title="Hosting Websites"
+          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque soluta
+          hic consequuntur eum repellendus ad."
+          icon={<FaGlobe />}
+        />
+
+            </div>
+
+        </section>
+    );
+};
+
+export default Features;
