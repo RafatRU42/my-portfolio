f9de559258dca968082fbe26e7a1e26c881e4dfa
Ruhul Muazzam Rafat <rmrafat420@gmail.com>
2023-03-17 08:42:57 +0600

    type writer completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Banner from './Components/Banner/Banner';
 
 function App() {
   return (
-    <div className="App">
-     <h1>this is tailwind</h1>
+    <div className="w-full h-auto bg-bodyColor text-lightText px-4">
+     <h1>this is navbar</h1>
+      <div className='max-w-screen-xl mx-auto'>
+        <Banner/>
+      </div>
     </div>
   );
 }
