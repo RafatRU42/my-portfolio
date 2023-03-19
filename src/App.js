2b9888062af4afaf6e4567cf3b39565d656a2da8
Ruhul Muazzam Rafat <rmrafat420@gmail.com>
2023-03-19 21:20:57 +0600

    contract and footer component added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
 import Banner from './Components/Banner/Banner';
+import Contact from './Components/Contract/Contact';
 import Features from './Components/Features/Features';
+import Footer from './Components/Footer/Footer';
+import FooterBottom from './Components/Footer/FooterBottom';
 import Projects from './Components/Projects/Projects';
 import Resume from './Components/Resume/Resume';
+import Testimonial from './Components/Testimonial/Testimonial';
 
 function App() {
   return (
@@ -13,6 +17,11 @@ function App() {
         <Features/>
         <Projects/>
         <Resume></Resume>
+        {/* <Testimonial></Testimonial> */}
+        <Contact/>
+        <Footer></Footer>
+        <FooterBottom/>
+       
       </div>
     </div>
   );
--- src/assets/index.js
+++ src/assets/index.js
@@ -2,10 +2,12 @@ import bannerImg from './images/editedprofile.png'
 import projectOne from './images/projects/projectOne.jpg'
 import projectTwo from './images/projects/projectTwo.jpg'
 import projectThree from './images/projects/projectThree.jpeg'
+import contactImg from './images/contact/contactImg.png'
 
 export {
     bannerImg,
     projectOne,
     projectTwo,
-    projectThree
+    projectThree,
+    contactImg,
 }
